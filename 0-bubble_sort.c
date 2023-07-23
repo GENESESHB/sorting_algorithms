c5a028ae4839ac7138261db7892337cdd99ce265
HASSANBOUDRRA <hassan.hbmama@gmail.com>
2023-07-23 04:55:36 -0700

    the bubble sort using algo for  sort the array

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,32 +2,47 @@
 #include <stdlib.h>
 #include "sort.h"
 
+/**
+ *swap - a function that swaps two integers.
+ *@a: first integer to swap.
+ *@b: second integer to swap.
+ *
+ * Return: void.
+ */
 
 void swap(int *a, int *b)
 {
 	int x;
-       	x = *a;
+
+	x = *a;
 	*a = *b;
 	*b = x;
 }
 
+/**
+ *bubble_sort - a function that sorts an array using
+ *using bubble sort algorithm.
+ *@array: the array to be sorted.
+ *@size: the size of the array.
+ *Return: void.
+ */
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i,j;
+	size_t i, j;
 
 	if (array == NULL || size == 0)
 		return;
 
-    for (i = size - 1; i > 0; i--)
-    {
-        for (j = 1; j <= i; j++)
+	for (i = size - 1; i > 0; i--)
 	{
-		if (array[j - 1] > array[j])
+		for (j = 1; j <= i; j++)
 		{
-			swap(&array[j - 1], &array[j]);
-			print_array(array, size);
+			if (array[j - 1] > array[j])
+			{
+				swap(&array[j - 1], &array[j]);
+				print_array(array, size);
+			}
 		}
 	}
-    }
 }
