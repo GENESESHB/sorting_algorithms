c01040ff44b6fd1ecd90a6183d54a9dffb8d639c
HASSANBOUDRRA <hassan.hbmama@gmail.com>
2023-07-23 05:34:33 -0700

    the algorithm select with sorted array

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,30 +1,32 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
+ *swap - Swap two integers in an array.
+ *@a: The first integer to swap.
+ *@b: The second integer to swap.
  */
+
 void swap(int *a, int *b)
 {
-	int tmp;
+	int x;
 
-	tmp = *a;
+	x = *a;
 	*a = *b;
-	*b = tmp;
+	*b = x;
 }
 
 /**
  * selection_sort - Sort an array of integers in ascending order
- *                  using the selection sort algorithm.
+ * using the selection sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
  *
- * Description: Prints the array after each swap.
+ * Description:prints the array after each swap.
  */
+
 void selection_sort(int *array, size_t size)
 {
-	int *min;
+	int *nsghir;
 	size_t i, j;
 
 	if (array == NULL || size < 2)
@@ -32,13 +34,13 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min = array + i;
+		nsghir = array + i;
 		for (j = i + 1; j < size; j++)
-			min = (array[j] < *min) ? (array + j) : min;
+			nsghir = (array[j] < *nsghir) ? (array + j) : nsghir;
 
-		if ((array + i) != min)
+		if ((array + i) != nsghir)
 		{
-			swap(array + i, min);
+			swap(array + i, nsghir);
 			print_array(array, size);
 		}
 	}
